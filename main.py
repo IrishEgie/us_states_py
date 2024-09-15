e066cbf432329b479a0f62445a73ff1246504f1a
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-15 11:51:04 +0800

    update: added image & text input

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import pandas as pd
-from turtle import Turtle, Screen
+import turtle
 
-screen = Screen()
+screen = turtle.Screen()
 screen.title("US States Game")
 # screen.setup(width=600, height=600)
 
 image = "blank_states_img.gif"
 screen.addshape(image)
 
-turtle = Turtle()
-
 def get_mouse_click_coor(x,y):
     print(x,y)
 
-
 turtle.shape(image)
 
-screen.exitonclick()
+answer_state = turtle.textinput(title="Guess a state!",prompt="What's another State?")
