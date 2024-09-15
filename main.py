0d8b663f555b95423df447ecf4c109d14d49126a
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-15 15:24:32 +0800

    fin: final commit after creating the quiz state game

--- main.py
+++ main.py
@@ -3,14 +3,42 @@ import turtle
 
 screen = turtle.Screen()
 screen.title("US States Game")
-# screen.setup(width=600, height=600)
 
 image = "blank_states_img.gif"
 screen.addshape(image)
-
-def get_mouse_click_coor(x,y):
-    print(x,y)
-
 turtle.shape(image)
 
-answer_state = turtle.textinput(title="Guess a state!",prompt="What's another State?")
+def read_data_csv():
+    data = pd.read_csv("50_states.csv")
+    return data
+
+def write_state_name(state_name, x, y):
+    pen = turtle.Turtle()
+    pen.hideturtle()
+    pen.penup()
+    pen.goto(x, y)
+    pen.write(state_name, align="center", font=("Arial", 8, "normal"))
+
+def state_matching(answer):
+    data = read_data_csv()
+    state_names = data["state"].to_list()
+
+    if answer in state_names:
+        state_row = data[data["state"] == answer]
+        x = int(state_row["x"])
+        y = int(state_row["y"])
+        write_state_name(answer, x, y)
+        return True
+    else:
+        return False
+# Main loop
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(f"{len(guessed_states)}/50", "Guess a state!")
+
+    if state_matching(answer_state) and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+    else:
+        print("Oops, that is incorrect!")
+        break
+screen.exitonclick()
