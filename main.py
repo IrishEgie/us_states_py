c854893fab84a519264014da0a414f87da038e58
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-15 16:18:35 +0800

    update: added a break  function that creates a states to learn file for unguessed states upon exit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def write_state_name(state_name, x, y):
 def state_matching(answer):
     data = read_data_csv()
     state_names = data["state"].to_list()
-
+    
     if answer in state_names:
         state_row = data[data["state"] == answer]
         x = int(state_row["x"])
@@ -31,14 +31,31 @@ def state_matching(answer):
         return True
     else:
         return False
+    
+def to_learn():
+        data = read_data_csv()
+        unlearnt_states = data["state"].to_list()
+
+        for state in guessed_states:
+            unlearnt_states.remove(state)
+        print(unlearnt_states, f"You still have to learn {len(unlearnt_states)} more states")
+        unlearnt_states_df = pd.DataFrame(unlearnt_states)
+        unlearnt_states_df.to_csv("states_to_learn.csv")
+
 # Main loop
 guessed_states = []
-while len(guessed_states) < 50:
-    answer_state = screen.textinput(f"{len(guessed_states)}/50", "Guess a state!")
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(f"{len(guessed_states)}/50", "Guess a state!").title()
+    
+    if answer_state == "Exit":
+        to_learn()
+        break
+    if guessed_states == 50:
+        print("Congratulations! You answered all the 50 states correctly!")
+        
     if state_matching(answer_state) and answer_state not in guessed_states:
         guessed_states.append(answer_state)
+
     else:
         print("Oops, that is incorrect!")
-        break
-screen.exitonclick()
